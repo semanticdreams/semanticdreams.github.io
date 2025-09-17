01179ed7bdf81d9f424baac3cae9bc32a0657bdf
semanticdreams <188935636+semanticdreams@users.noreply.github.com>
2025-09-17 13:05:07 +0300

    Update

--- .vitepress/theme/components/ProjectCard.vue
+++ .vitepress/theme/components/ProjectCard.vue
@@ -1,7 +1,7 @@
 <template>
   <a :href="href" class="w-full">
     <div class="p-1 flex md:flex-row w-full gap-2 rounded-2xl border-2 border-[var(--vp-c-border)] hover:bg-[var(--vp-c-bg-soft)]">
-        <img class="m-1 w-12 h-12 sm:w-12 sm:h-12" :src="image" :alt="title" />
+        <img class="m-1 w-12 h-12 sm:w-12 sm:h-12" :src="withBase(image)" :alt="title" />
       <div class="flex flex-col gap-0">
         <div class="text-base font-bold">
           {{ title }}
@@ -14,25 +14,25 @@
   </a>
 </template>
 
-<script>
-export default {
-    props: {
-        title: {
-            type: String,
-            required: true,
-        },
-        description: {
-            type: String,
-            required: true,
-        },
-        image: {
-            type: String,
-            required: true,
-        },
-        href: {
-            type: String,
-            required: true,
-        },
-    },
-};
+<script setup>
+import { withBase } from 'vitepress'
+
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  href: {
+    type: String,
+    required: true,
+  },
+})
 </script>
