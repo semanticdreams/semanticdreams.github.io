c11cda4e4dbf57be48f696bd6f3d71f2ad8e2418
semanticdreams <188935636+semanticdreams@users.noreply.github.com>
2025-09-26 10:58:52 +0300

    Update

--- .vitepress/theme/components/ProjectCard.vue
+++ .vitepress/theme/components/ProjectCard.vue
@@ -1,7 +1,7 @@
 <template>
   <a :href="href" class="w-full">
     <div class="p-1 flex md:flex-row w-full gap-2 rounded-2xl border-2 border-[var(--vp-c-border)] hover:bg-[var(--vp-c-bg-soft)]">
-        <img class="m-1 w-12 h-12 sm:w-12 sm:h-12" :src="withBase(image)" :alt="title" />
+        <img class="m-1 w-12 h-12 sm:w-12 sm:h-12 aspect-square" :src="withBase(image)" :alt="title" />
       <div class="flex flex-col gap-0">
         <div class="text-base font-bold">
           {{ title }}
